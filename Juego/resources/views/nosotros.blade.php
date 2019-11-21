6c18554416c3e974202a15cd2cf4791b9192ec87
TomasWettstein <tomas_fw@hotmail.com>
2019-11-21 17:06:55 -0300

    nosotros y contacto, vistas

---
+++ Juego/resources/views/nosotros.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.plantillaA')
+@section('contenido')
+<section class="container">
+<section class="sec--contacto">
+    <p class="p--contacto">
+        Nosotros
+    </p>
+    <div class="caja--contacto">
+       <center> <img src="../img/Iconos-Cosas-Varias/Titulo recto.png" width="200px" class="img--contacto"></center>
+        <p class="p--caja">
+           <br> Es un juego creado por un grupo de estudiantes de Digital House.<br>
+            Lo forman: Agustina quien es Scrum master junto con Mauricio y Tomas que son parte del Scrum Team.
+           <br> Esperamos que puedan acceder a nuestro juegos con facilidad y que puedan disfrutarlos, si necesitan contactarse 
+            con nosotross pueden presionar el boton de abajo o tambien pueden seguirnos en nuestras redes sociales.
+        </p>
+    </div>
+    <div class="caja--contactenos">
+        <button onclick="location.href='contacto.php'" class="b_contactenos"> Contactenos </button>
+    </div>
+
+    
+</section>
+
+    
+@endsection
--- Juego/resources/views/usuarios.blade.php
+++ Juego/resources/views/usuarios.blade.php
@@ -1,1 +1,6 @@
 @extends('layouts.plantillaA')
+@section('contenido')
+{{$usuarios[3]->nombre}}
+{{$usuarios[3]->pregunta[0]['pregunta']}}
+ 
+@endsection
