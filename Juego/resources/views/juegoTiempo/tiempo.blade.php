828897022f7f0f6b7e1e724b987c7128e3a34db8
Agustina Maraz <agus_maraz@hotmail.com>
2019-12-06 15:54:56 -0300

    fusion

--- Juego/resources/views/juegoTiempo/tiempo.blade.php
+++ Juego/resources/views/juegoTiempo/tiempo.blade.php
@@ -1,28 +1,34 @@
 @extends('layouts.plantillaA')
 @section('contenido')
 <section class="container">
-    <div class="cajita-juegoTiempo">     
+    {{-- <div class="cajita-puntos">
         puntos:  {{session()->get('puntosPartida')}}
         vidas: {{session()->get('vidas')}}
-        <p class="preg-juego">
-            {{-- {{$preguntas->pregunta}} --}}
-            {{$pregunta->pregunta}}
-        </p> 
-        
-        <form action="{{url('siguiente')}}" method="post" id="respuesta-1">
-            @csrf
-            <input type="submit" name="respuesta-tiempo" value="{{$pregunta->Respuesta['correcta']}}" class="respuesta-juegoA">
-        </form>
-        
-        <form action="{{url('siguiente')}}" method="post" id="respuesta-2">
-            @csrf
-            <input type="submit" name="respuesta-tiempo" value="{{$pregunta->Respuesta['falsa1']}}" class="respuesta-juegoB">
-        </form>
-        
-        <form action="{{url('siguiente')}} " method="post" id="respuesta-3">
-            @csrf
-            <input type="submit" name="respuesta-tiempo" value="{{$pregunta->Respuesta['falsa2']}}" class="respuesta-juegoC">
-        </form>
+    </div> --}}
+    <div class="cajita-juegoTiempo">     
+        <ul type='none'>
+            <li class="pregunta-juego">
+                <p class="preg-juego">
+                    {{-- {{$preguntas->pregunta}} --}}
+                    {{$pregunta->pregunta}}
+                </p> 
+            </li>
+            
+            <form action="{{url('siguiente')}}" method="post" id="respuesta-1">
+                @csrf
+                <input type="submit" name="respuesta-tiempo" value="{{$pregunta->Respuesta['correcta']}}" class="respuesta-juegoA">
+            </form>
+            
+            <form action="{{url('siguiente')}}" method="post" id="respuesta-2">
+                @csrf
+                <input type="submit" name="respuesta-tiempo" value="{{$pregunta->Respuesta['falsa1']}}" class="respuesta-juegoB">
+            </form>
+            
+            <form action="{{url('siguiente')}} " method="post" id="respuesta-3">
+                @csrf
+                <input type="submit" name="respuesta-tiempo" value="{{$pregunta->Respuesta['falsa2']}}" class="respuesta-juegoC">
+            </form>
+        </ul>
         
     </div>
     
